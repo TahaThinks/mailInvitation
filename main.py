8b97866cfb9a333d442ebdff71c615814804f037
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-03 10:58:10 +0400

    Create Letter for one Invitee

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ with open("starting_letter.txt") as file:
         starting_contents.append(line.rstrip())
 
 # Line to change at position[0] of starting_content
+        
+with open(f"ReadyToSend/{contents[0]}.txt", "w") as file:
+    
+    starting_contents[0] = f"Dear {contents[0]},"
+    
+    for line in starting_contents:
+        file.write(f"{line}\n")
